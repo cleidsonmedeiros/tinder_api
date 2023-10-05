8e38276c40c1e06cf59777cb5be521984223e8f6
cleidsonmedeiros <cleidsonmedeiros12@gmail.com>
2023-10-05 05:14:58 -0300

    implement database

---
+++ db/migrate/20231005081350_create_matches.rb
@@ -0,0 +1,11 @@
+class CreateMatches < ActiveRecord::Migration[7.0]
+  def change
+    create_table :matches do |t|
+      t.references :user1, null: false, foreign_key: true
+      t.references :user2, null: false, foreign_key: true
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
